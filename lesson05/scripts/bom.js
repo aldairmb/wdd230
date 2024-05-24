12acddad2dda55cb7684622caf09cb8af70d99cb
aldairmb <aldairmorales.banda@gmail.com>
2024-05-23 19:44:52 -0600

    bug

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,31 @@
+const favchap = document.querySelector("#favchap")
+const submitbutton = document.querySelector("#addbutton")
+const mylist = document.querySelector("#list")
+
+submitbutton.addEventListener("click", () =>{
+    if(favchap.vaue == ""){
+        console.log("You didn't type anything")
+        favchap.focus;
+        return;
+    }
+
+    let newitem = document.createElement("li")
+
+    let deletebutton = document.createComment("button")
+
+    newitem.innerText = favchap.value
+
+    deletebutton.innerText = "X"
+
+    newitem.append(deletebutton)
+
+    mylist.append(newitem)
+
+    deletebutton.addEventListener("click", () => {
+        newitem.remove()
+        favchap,focus()
+    })
+
+    favchap.value = ""
+
+})
