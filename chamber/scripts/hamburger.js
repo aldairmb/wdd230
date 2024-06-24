31132a8edc381828e9c3c8c3ee7a1720d9c236e3
aldairmb <aldairmorales.banda@gmail.com>
2024-06-23 21:34:41 -0600

    nav

---
+++ chamber/scripts/hamburger.js
@@ -0,0 +1,8 @@
+// Hamburger menu
+
+let menubutton = document.getElementById("menubutton")
+let navbar = document.getElementById("navbar")
+menubutton.addEventListener("click", () => {
+    menubutton.classList.toggle("open")
+    navbar.classList.toggle("open")
+})
